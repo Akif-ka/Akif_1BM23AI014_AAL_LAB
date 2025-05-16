35b35593f479238028ca4bd330ff065815c5c1f3
Akif karuvath <akifkaruvath.ai23@bmsce.ac.in>
2025-05-16 17:42:32 +0300

    Update LAB 8(floyd's algorithm).c

--- LAB 8(floyd's algorithm).c
+++ LAB 8(floyd's algorithm).c
@@ -1,19 +1,13 @@
 #include <stdio.h>
-
 #define V 4
 #define INF 99999
-
 void floydWarshall(int graph[V][V]) {
     int dist[V][V];
-
-
     for (int i = 0; i < V; i++) {
         for (int j = 0; j < V; j++) {
             dist[i][j] = graph[i][j];
         }
     }
-
-
     for (int k = 0; k < V; k++) {
         for (int i = 0; i < V; i++) {
             for (int j = 0; j < V; j++) {
@@ -22,8 +16,6 @@ void floydWarshall(int graph[V][V]) {
             }
         }
     }
-
-
     printf("Shortest distances between every pair of vertices:\n");
     for (int i = 0; i < V; i++) {
         for (int j = 0; j < V; j++) {
@@ -35,15 +27,21 @@ void floydWarshall(int graph[V][V]) {
         printf("\n");
     }
 }
-
 int main() {
     int graph[V][V] = {
-        {0,   5,  INF, 10},
-        {INF, 0,   3, INF},
+        {0,   13,  INF, 10},
+        {INF, 5,   6, INF},
         {INF, INF, 0,   1},
         {INF, INF, INF, 0}
     };
-
     floydWarshall(graph);
     return 0;
 }
+
+
+OUTPUT:
+Shortest distances between every pair of vertices:
+      0     13     19     10
+    INF      5      6      7
+    INF    INF      0      1
+    INF    INF    INF      0
