d3631d129c4c634527f65a4f9bf703dcc7f7a35b
Akif karuvath <akifkaruvath.ai23@bmsce.ac.in>
2025-05-16 17:28:37 +0300

    Update LAB 2(String matching using brute force).c

--- LAB 2(String matching using brute force).c
+++ LAB 2(String matching using brute force).c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <string.h>
-
 void bruteForceSearch(char text[], char pattern[]) {
     int n = strlen(text);
     int m = strlen(pattern);
     int found = 0;
-
     for (int i = 0; i <= n - m; i++) {
         int j;
         for (j = 0; j < m; j++) {
@@ -18,24 +16,24 @@ void bruteForceSearch(char text[], char pattern[]) {
             found = 1;
         }
     }
-
     if (!found) {
         printf("Pattern not found in the text.\n");
     }
 }
-
 int main() {
     char text[100], pattern[100];
-
     printf("Enter the text: ");
     fgets(text, sizeof(text), stdin);
     text[strcspn(text, "\n")] = '\0';
-
     printf("Enter the pattern to search: ");
     fgets(pattern, sizeof(pattern), stdin);
     pattern[strcspn(pattern, "\n")] = '\0';
-
     bruteForceSearch(text, pattern);
-
     return 0;
 }
+
+
+OUTPUT:
+Enter the text: AABBCCAABBCDABD
+Enter the pattern to search: BCD
+Pattern found at index 9
