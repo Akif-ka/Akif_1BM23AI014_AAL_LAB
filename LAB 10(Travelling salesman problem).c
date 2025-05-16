ab3075ca896bf521d91f55594c93573a77b67a94
Akif karuvath <akifkaruvath.ai23@bmsce.ac.in>
2025-05-16 10:35:32 +0530

    Update LAB 10(Travelling salesman problem).c

--- LAB 10(Travelling salesman problem).c
+++ LAB 10(Travelling salesman problem).c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 #include <limits.h>
-
 #define V 4
-
 int tsp(int graph[V][V], int visited[], int pos, int n, int count, int cost, int start) {
     if (count == n && graph[pos][start]) {
         return cost + graph[pos][start];
     }
-
     int ans = INT_MAX;
 
     for (int i = 0; i < n; i++) {
@@ -18,25 +15,23 @@ int tsp(int graph[V][V], int visited[], int pos, int n, int count, int cost, int
             visited[i] = 0;
         }
     }
-
     return ans;
 }
-
 int main() {
     int graph[V][V] = {
-        {0, 10, 15, 20},
-        {10, 0, 35, 25},
-        {15, 35, 0, 30},
-        {20, 25, 30, 0}
+        {0, 11, 15, 20},
+        {8, 0, 35, 25},
+        {16, 35, 40, 30},
+        {50, 45, 30, 0}
     };
-
     int visited[V];
     for (int i = 0; i < V; i++) visited[i] = 0;
-
     visited[0] = 1;
-
     int result = tsp(graph, visited, 0, V, 1, 0, 0);
     printf("Minimum cost of travelling all cities: %d\n", result);
-
     return 0;
 }
+
+
+OUTPUT:
+Minimum cost of travelling all cities: 82
