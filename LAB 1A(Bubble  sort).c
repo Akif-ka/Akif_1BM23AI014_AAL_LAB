31132162793792a4deeda465bbc468032837627f
Akif karuvath <akifkaruvath.ai23@bmsce.ac.in>
2025-05-16 10:36:56 +0530

    Update LAB 1A(Bubble  sort).c

--- LAB 1A(Bubble  sort).c
+++ LAB 1A(Bubble  sort).c
@@ -1,44 +1,40 @@
 #include <stdio.h>
-
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
-
         int swapped = 0;
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j + 1]
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
                 swapped = 1;
             }
         }
-
-
         if (!swapped)
             break;
     }
 }
-
 void printArray(int arr[], int n) {
     printf("Sorted array: ");
     for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
-
 int main() {
     int arr[100], n;
-
     printf("Enter number of elements: ");
     scanf("%d", &n);
-
     printf("Enter %d elements:\n", n);
     for (int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
-
     bubbleSort(arr, n);
     printArray(arr, n);
-
     return 0;
 }
+
+
+OUTPUT:
+Enter number of elements: 6
+Enter 6 elements:
+1 12 35 6 8 4
+Sorted array: 1 4 6 8 12 35 
