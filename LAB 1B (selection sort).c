e5b66bd3dd77580a996cf158f6f85512b4755997
Akif karuvath <akifkaruvath.ai23@bmsce.ac.in>
2025-05-16 10:38:06 +0530

    Update LAB 1B (selection sort).c

--- LAB 1B (selection sort).c
+++ LAB 1B (selection sort).c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
 void selectionSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         int minIndex = i;
-
-
         for (int j = i + 1; j < n; j++) {
             if (arr[j] < arr[minIndex])
                 minIndex = j;
         }
-
         if (minIndex != i) {
             int temp = arr[i];
             arr[i] = arr[minIndex];
@@ -17,26 +13,27 @@ void selectionSort(int arr[], int n) {
         }
     }
 }
-
 void printArray(int arr[], int n) {
     printf("Sorted array: ");
     for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
-
 int main() {
     int arr[100], n;
-
     printf("Enter number of elements: ");
     scanf("%d", &n);
-
     printf("Enter %d elements:\n", n);
     for (int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
-
     selectionSort(arr, n);
     printArray(arr, n);
-
     return 0;
 }
+
+
+OUTPUT:
+Enter number of elements: 4
+Enter 4 elements:
+12 5 6 80
+Sorted array: 5 6 12 80 
