0dd855e401397f6cbb0972ce6266ccc949e2442b
Akif karuvath <akifkaruvath.ai23@bmsce.ac.in>
2025-05-16 17:31:08 +0300

    Update LAB 3A(merge sort).c

--- LAB 3A(merge sort).c
+++ LAB 3A(merge sort).c
@@ -1,60 +1,47 @@
 #include <stdio.h>
-
 void merge(int arr[], int left, int mid, int right) {
     int n1 = mid - left + 1;
     int n2 = right - mid;
-
     int L[n1], R[n2];
-
-
     for (int i = 0; i < n1; i++)
         L[i] = arr[left + i];
     for (int j = 0; j < n2; j++)
         R[j] = arr[mid + 1 + j];
-
-
     int i = 0, j = 0, k = left;
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) arr[k++] = L[i++];
         else arr[k++] = R[j++];
     }
-
-
     while (i < n1) arr[k++] = L[i++];
-
-
     while (j < n2) arr[k++] = R[j++];
 }
-
 void mergeSort(int arr[], int left, int right) {
     if (left < right) {
         int mid = left + (right - left) / 2;
-
-
         mergeSort(arr, left, mid);
         mergeSort(arr, mid + 1, right);
-
-
         merge(arr, left, mid, right);
     }
 }
-
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
-
 int main() {
-    int arr[] = {12, 11, 13, 5, 6, 7};
+    int arr[] = {30, 101, 47, 96, 11, 55};
     int n = sizeof(arr) / sizeof(arr[0]);
-
     printf("Original array:\n");
     printArray(arr, n);
-
     mergeSort(arr, 0, n - 1);
-
     printf("Sorted array:\n");
     printArray(arr, n);
     return 0;
 }
+
+
+OUTPUT:
+Original array:
+30 101 47 96 11 55 
+Sorted array:
+11 30 47 55 96 101 
